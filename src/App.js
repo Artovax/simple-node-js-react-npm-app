9ce9bf16a6c2dc7c7c5e85afd09d3fbd7d53dd39
Artovax <209798@student.pwr.edu.pl>
2022-11-07 01:36:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Cześć Kocham Pieski . Ta strona jest uruchamiana przez pipeline jenkinsa
+           na 13.73.148.81:8080  OCB </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
